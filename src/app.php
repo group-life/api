c920c8edf76283c01773d8f242cff1ee690ec8ad
Magomogo <mgnatenko@gmail.com>
2021-10-15 11:14:40 +0300

    Extract App configuration into a file

---
+++ src/app.php
@@ -0,0 +1,14 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+return $app;
