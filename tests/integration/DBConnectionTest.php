3656f7c87ab5087193a9da79c079867a99266b3e
Lehersun <kpekep@list.ru>
2021-10-21 12:10:11 +0300

    Fix DB connection test

--- tests/integration/DBConnectionTest.php
+++ tests/integration/DBConnectionTest.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 namespace GroupLife\Api\Tests\integration;
 
-use GroupLife\Api\Database\DBConnection;
 use PHPUnit\Framework\TestCase;
 
 class DBConnectionTest extends TestCase
 {
     public function testConnectionToDB(): void
     {
-        $db = DBConnection::connect();
+        $db = database();
         self::assertEquals('test.db', basename($db->getDatabase()));
 
     }
@@ -21,7 +20,7 @@ class DBConnectionTest extends TestCase
      */
     public function testInsertValueToDB(): void
     {
-        $db = DBConnection::connect();
+        $db = database();
         $db->insert('visitor', ['name' => 'Ivan', 'surname' => 'Petrov']);
         $result = $db->fetchAssociative('SELECT * FROM visitor');
         self::assertEquals(
